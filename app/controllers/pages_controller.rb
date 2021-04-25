b79ef96613f21638bc77aa36c5f5443a00c28ddc
Cruz <cruzmax21@gmail.com>
2021-04-25 16:54:48 -0700

    controllers

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,27 @@
+class PagesController < ApplicationController
+  def about
+
+  end
+
+  def one
+
+  end
+
+  def two
+
+  end
+
+  def three
+
+  end
+
+  def new
+
+  end
+
+
+  def create
+      @messages = (params[:messages])
+  end
+
+end
